e9daa0c1e3b17fe81688be5006ab253c4bf6d3cd
Mesi <mse212002@gmail.com>
2020-02-12 18:52:05 +0200

    :art: Fixed stickler errors

--- src/modules/header.js
+++ src/modules/header.js
@@ -1,38 +1,37 @@
 const fullHeader = () => {
-    const mainHeader = document.createElement('header');
-    mainHeader.setAttribute('class', 'container-fluid')
-    mainHeader.setAttribute('id', 'header')
-    
-    const left = document.createElement('div');
-    left.setAttribute('id', 'left-h');
-    
-    const right = document.createElement('div');
-    right.setAttribute('id', 'right-h');
+  const mainHeader = document.createElement('header');
+  mainHeader.setAttribute('class', 'container-fluid');
+  mainHeader.setAttribute('id', 'header');
 
-    const restaurantName = document.createElement('h1');
-    restaurantName.setAttribute('id', 'restName');
-    restaurantName.innerHTML = 'Eat, Drink, Enjoy';
-    left.appendChild(restaurantName);
-    mainHeader.appendChild(left);
+  const left = document.createElement('div');
+  left.setAttribute('id', 'left-h');
 
-    const navBar = document.createElement('ul');
-    navBar.setAttribute('id', 'navbar');
-    navBar.setAttribute('class', 'list-group')
+  const right = document.createElement('div');
+  right.setAttribute('id', 'right-h');
 
-    let navTabs = ['About', 'Menu', 'Staff', 'Contact'];
+  const restaurantName = document.createElement('h1');
+  restaurantName.setAttribute('id', 'restName');
+  restaurantName.innerHTML = 'Eat, Drink, Enjoy';
+  left.appendChild(restaurantName);
+  mainHeader.appendChild(left);
 
-    navTabs.forEach((tab) => {
-        let tabName = document.createElement('li');
-        tabName.setAttribute('id', `${tab}`);
-        tabName.innerHTML = `${tab}`;
-        navBar.appendChild(tabName);   
-    })
+  const navBar = document.createElement('ul');
+  navBar.setAttribute('id', 'navbar');
+  navBar.setAttribute('class', 'list-group')
 
-    right.appendChild(navBar);
-    mainHeader.appendChild(right);
+  let navTabs = ['About', 'Menu', 'Staff', 'Contact'];
 
-    return mainHeader; 
-    
+  navTabs.forEach((tab) => {
+    let tabName = document.createElement('li');
+    tabName.setAttribute('id', `${tab}`);
+    tabName.innerHTML = `${tab}`;
+    navBar.appendChild(tabName);   
+  });
+
+  right.appendChild(navBar);
+  mainHeader.appendChild(right);
+
+  return mainHeader;   
 }
 
 export default fullHeader;
